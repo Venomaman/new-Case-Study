bc360f8f67f63b221e3dc31c5793da8c6c46995e
Venomaman <61680390+Venomaman@users.noreply.github.com>
2022-05-01 23:23:42 +0530

    Add files via upload
    checkin

---
+++ CheckIn/src/main/java/com/check/controller/Controller.java
@@ -0,0 +1,63 @@
+package com.check.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.check.model.Booking;
+import com.check.service.EmailService;
+
+@RestController
+@RequestMapping("/attendee")
+public class Controller {
+	
+	//Rest Template
+		@Autowired
+		private RestTemplate restTemplate;
+		@Autowired
+		EmailService emailService;
+		
+		
+		
+		//Put Booking checkIn=true for  updating the Checked-In status
+		
+		@PutMapping("/booking/{booking_id}")
+		
+	//	  @HystrixCommand(fallbackMethod = "UpdateBookingFallback")
+		 	public String updateBooking (@RequestBody Booking booking,@PathVariable("booking_id") long pnr) throws InterruptedException {
+			booking.setChecked_in(true);
+		restTemplate.put("https://Flight-Booking/FlightBooking/booking/"+pnr, booking);
+		/* return"Checked In Successfully:"+pnr ; */
+		 Thread.sleep(3000);
+		return emailService.sendEmail(booking);
+			
+		}
+		
+		
+		  private String UpdateBookingFallback(Booking booking,long pnr) { 
+			  return "Request fails. It takes long time to response"; }
+		 
+		
+		//Get Booking  for  attendee to fetch data of user from Booking 
+
+		@GetMapping("/BookedFlight/{booking_Id}")
+	//	@HystrixCommand(fallbackMethod = "GetBookingFallback")
+		public Booking getBooking(@PathVariable("booking_Id") long booking_id) throws InterruptedException {
+		Booking booking=restTemplate.getForObject("https://Flight-Booking/FlightBooking/BookedFlight/"+booking_id, Booking.class);
+		Thread.sleep(3000);
+			return booking ;
+			
+		}
+		
+		private Booking GetBookingFallback(@PathVariable long booking_id) {
+			return null;
+			 
+			
+			}
+
+}
